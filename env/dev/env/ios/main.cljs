e3de875b342da8e999e271be916d87e127cd4610
Matt Fellows <matt.fellows@onegeek.com.au>
2016-09-17 08:43:07 +1000

    wip(upgrade-react): upgraded to latest react controllers

--- env/dev/env/ios/main.cljs
+++ env/dev/env/ios/main.cljs
@@ -1,49 +1,49 @@
+; (ns ^:figwheel-no-load env.ios.main
+;  (:require [reagent.core :as r]
+;            [yimp.ios.core :as core]
+;            [figwheel.client :as figwheel :include-macros true]))
+;
+; (enable-console-print!)
+; (print "aoeuaoeus")
+; (def cnt (r/atom 0))
+; (defn reloader [] @cnt [core/app-root])
+; (def root-el (r/as-element [reloader]))
+;
+; (figwheel/watch-and-reload
+; :websocket-url "ws://localhost:3449/figwheel-ws"
+; :heads-up-display false
+; :jsload-callback #(swap! cnt inc))
+;
+; (core/init)
 (ns ^:figwheel-no-load env.ios.main
- (:require [reagent.core :as r]
-           [yimp.ios.core :as core]
-           [figwheel.client :as figwheel :include-macros true]))
+  (:require [reagent.core :as r]
+            [yimp.ios.core :as core]
+            [figwheel.client :as figwheel :include-macros true]
+            [yimp.utils :as u]
+            [yimp.handlers]
+            [yimp.subs]
+            [yimp.ios.screens.home :as home]))
 
 (enable-console-print!)
-(print "aoeuaoeus")
+(aset js/console "disableYellowBox" true)
+
+(def home (:component home/home))
+
 (def cnt (r/atom 0))
-(defn reloader [] @cnt [core/app-root])
+(defn reloader []
+  @cnt
+  [home])
+
+
 (def root-el (r/as-element [reloader]))
 
 (figwheel/watch-and-reload
-:websocket-url "ws://localhost:3449/figwheel-ws"
-:heads-up-display false
-:jsload-callback #(swap! cnt inc))
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :heads-up-display false
+  :jsload-callback (fn []
+                     (u/clear-console!)
+                     (core/init)
+                     (swap! cnt inc)))
 
+(u/clear-console!)
 (core/init)
-; (ns ^:figwheel-no-load env.ios.main
-;   (:require [reagent.core :as r]
-;             [yimp.ios.core :as core]
-;             [figwheel.client :as figwheel :include-macros true]
-;             [yimp.utils :as u]
-;             [yimp.handlers]
-;             [yimp.subs]            
-;             [yimp.ios.screens.home :as home]))
-; 
-; (enable-console-print!)
-; (aset js/console "disableYellowBox" true)
-; 
-; (def home (:component home/home))
-; 
-; (def cnt (r/atom 0))
-; (defn reloader []
-;   @cnt
-;   [home])
-; 
-; 
-; (def root-el (r/as-element [reloader]))
-; 
-; (figwheel/watch-and-reload
-;   :websocket-url "ws://localhost:3449/figwheel-ws"
-;   :heads-up-display false
-;   :jsload-callback (fn []
-;                      (u/clear-console!)
-;                      (core/init-nav)
-;                      (swap! cnt inc)))
-; 
-; (u/clear-console!)
-; (core/init)
--- src/yimp/ios/core.cljs
+++ src/yimp/ios/core.cljs
@@ -1,85 +1,85 @@
+; (ns yimp.ios.core
+;   (:require [reagent.core :as r :refer [atom]]
+;             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+;             [yimp.subs]))
+;
+; (def ReactNative (js/require "react-native"))
+;
+; (def app-registry (.-AppRegistry ReactNative))
+; (def text (r/adapt-react-class (.-Text ReactNative)))
+; (def view (r/adapt-react-class (.-View ReactNative)))
+; (def image (r/adapt-react-class (.-Image ReactNative)))
+; (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+;
+; (def logo-img (js/require "./images/cljs.png"))
+;
+; (defn alert [title]
+;       (.alert (.-Alert ReactNative) title))
+;
+; (defn app-root []
+;   ; (let [greeting (subscribe [:get-greeting])]
+;   (let [greeting "aoeu"]
+;     (fn []
+;       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+;        [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} greeting]
+;        [image {:source logo-img
+;                :style  {:width 80 :height 80 :margin-bottom 30}}]
+;        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+;                              :on-press #(alert "HELLO!")}
+;         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+;
+; (defn init []
+;       ; (dispatch-sync [:initialize-db])
+;       (.registerComponent app-registry "yimp" #(r/reactify-component app-root)))
+
+
 (ns yimp.ios.core
-  (:require [reagent.core :as r :refer [atom]]
-            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-            [yimp.subs]))
+  (:require [print.foo :as pf :include-macros true]
+            [schema.core :as s :include-macros true]
+            [re-frame.core :as rf]
+            [yimp.db]
+            [yimp.handlers]
+            [yimp.subs]
+            [yimp.shared.navigation :as nav]
+            [yimp.ios.screens.home :refer [home]]
+            [yimp.ios.screens.teachers :refer [teachers]]
+            [yimp.ios.screens.students :refer [students]]
+            [yimp.ios.screens.incidents :refer [incidents]]
+            [yimp.ios.screens.detail :refer [detail]]
+            [yimp.ios.screens.menu :refer [menu]]
+            [yimp.ios.screens.edit-incident :refer [edit-incident]]
+            [yimp.ios.screens.user :refer [user]]
+            [yimp.shared.ui :as ui]))
 
-(def ReactNative (js/require "react-native"))
-
-(def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
-
-(def logo-img (js/require "./images/cljs.png"))
-
-(defn alert [title]
-      (.alert (.-Alert ReactNative) title))
+(s/set-fn-validation! goog.DEBUG)
+(def nav-content-color (ui/color :orange800))
 
 (defn app-root []
-  ; (let [greeting (subscribe [:get-greeting])]
-  (let [greeting "aoeu"]
-    (fn []
-      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} greeting]
-       [image {:source logo-img
-               :style  {:width 80 :height 80 :margin-bottom 30}}]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+  (let [page (rf/subscribe [:current-page])
+        u (rf/subscribe [:user])
+        username (:username @u)]
+    (nav/register-screen! home)
+    (nav/register-screen! teachers)
+    (nav/register-screen! students)
+    (nav/register-screen! incidents)
+    (nav/register-screen! detail)
+    (nav/register-screen! edit-incident)
+    (nav/register-screen! user)
+    (nav/register-reagent-component! :menu menu)
+    (nav/start-single-screen-app!
+      {:screen          (if (not= username "") @page :user)
+       :drawer          {:left                 {:screen :menu}
+                         :disable-open-gesture true}
+       :persist-state?  true
+       :animationType   :fade
+       :navigator-style {:nav-bar-blur         true
+                         :draw-under-nav-bar   true
+                         :nav-bar-button-color nav-content-color
+                        ;  :nav-bar-background-color "#efefef"
+                         :nav-bar-text-color   nav-content-color}})))
 
 (defn init []
-      ; (dispatch-sync [:initialize-db])
-      (.registerComponent app-registry "yimp" #(r/reactify-component app-root)))
-
-
-; (ns yimp.ios.core
-;   (:require [print.foo :as pf :include-macros true]
-;             [schema.core :as s :include-macros true]
-;             [re-frame.core :as rf]
-;             [yimp.db]
-;             [yimp.handlers]
-;             [yimp.subs]
-;             [yimp.shared.navigation :as nav]
-;             [yimp.ios.screens.home :refer [home]]
-;             [yimp.ios.screens.teachers :refer [teachers]]
-;             [yimp.ios.screens.students :refer [students]]
-;             [yimp.ios.screens.incidents :refer [incidents]]
-;             [yimp.ios.screens.detail :refer [detail]]
-;             [yimp.ios.screens.menu :refer [menu]]
-;             [yimp.ios.screens.edit-incident :refer [edit-incident]]
-;             [yimp.ios.screens.user :refer [user]]
-;             [yimp.shared.ui :as ui]))
-; 
-; (s/set-fn-validation! goog.DEBUG)
-; (def nav-content-color (ui/color :orange800))
-; 
-; (defn app-root []
-;   (let [page (rf/subscribe [:current-page])
-;         u (rf/subscribe [:user])
-;         username (:username @u)]
-;     (nav/register-screen! home)
-;     (nav/register-screen! teachers)
-;     (nav/register-screen! students)
-;     (nav/register-screen! incidents)
-;     (nav/register-screen! detail)
-;     (nav/register-screen! edit-incident)
-;     (nav/register-screen! user)
-;     (nav/register-reagent-component! :menu menu)
-;     (nav/start-single-screen-app!
-;       {:screen          (if (not= username "") @page :user)
-;        :drawer          {:left                 {:screen :menu}
-;                          :disable-open-gesture true}
-;        :persist-state?  true
-;        :animationType   :fade
-;        :navigator-style {:nav-bar-blur         true
-;                          :draw-under-nav-bar   true
-;                          :nav-bar-button-color nav-content-color
-;                         ;  :nav-bar-background-color "#efefef"
-;                          :nav-bar-text-color   nav-content-color}})))
-; 
-; (defn init []
-;   (rf/dispatch-sync [:initialize-db])
-;   (app-root)
-;   (rf/dispatch [:load-students])
-;   (rf/dispatch [:load-incidents]))
+  (rf/dispatch-sync [:initialize-db])
+  (app-root)
+  (rf/dispatch [:load-students])
+  (rf/dispatch [:load-incidents]))
